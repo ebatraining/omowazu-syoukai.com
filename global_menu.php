1646f1ed5941fb0c54198d0928be03b925b7d357
DESKTOP-IO250O6\EBA <t.hamada@ebacorp.jp>
2017-06-20 13:06:50 +0900

    共通ヘッダーのglobal_menu.phpファイルを作成

---
+++ global_menu.php
@@ -0,0 +1,13 @@
+<div class="background">
+	<div class="max">
+		<div class="logo">
+			<a href="index.php"><img src="img/logo.png" width="145" height="68" alt=""></a>
+		</div>
+		<ul>
+			<li><a href="index.php"><img src="img/button_top.png" width="140" height="40" alt="TOP"></a></li>
+			<li><a href="kuchikomi01.php"><img src="img/button_samazama.png" width="140" height="40" alt="様々なサイト"></a></li>
+			<li><a href="about.php"><img src="img/button_about.png" width="140" height="40" alt="このサイトについて"></a></li>
+			<li><a href="contact.php"><img src="img/button_contact.png" width="140" height="40" alt="お問い合わせ"></a></li>
+		</ul>
+	</div>
+</div>
