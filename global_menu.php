23af099aa2181930b86c18b8e56da958e347ac54
DESKTOP-IO250O6\EBA <t.hamada@ebacorp.jp>
2017-06-22 18:00:56 +0900

    共通ヘッダー内ナビ　リンクのテキストを修正

--- global_menu.php
+++ global_menu.php
@@ -3,7 +3,7 @@
 			<h1><img src="./img/logo.png" alt="思わず紹介.comのロゴ"></h1>
 			<nav>
 				<ul>
-					<li><a href="about.php"><i class="fa fa-question-circle-o" aria-hidden="true"></i>このサイトについて</a></li>
+					<li><a href="about.php"><i class="fa fa-question-circle-o" aria-hidden="true"></i>サイトについて</a></li>
 					<li><a href="kuchikomi01.php"><i class="fa fa-window-maximize" aria-hidden="true"></i>様々なサイト</a></li>
 					<li><a href="contact.php"><i class="fa fa-envelope-o" aria-hidden="true"></i>お問い合わせ</a></li>
 				</ul>
